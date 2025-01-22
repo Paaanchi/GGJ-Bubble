530407aac36e93406436ee1532af6091683e09d4
Paaanchi <j.chepechvez@uandresbello.edu>
2025-01-22 16:21:09 -0300

    22-01
    Disparo
    enemigo

---
+++ Assets/Scripts/Shooter/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject attack;
+    public Transform PuntoDisparo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.rotation(0, -90, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject AtaqueInstanciada = Instantiate(attack, PuntoDisparo.position, PuntoDisparo.rotation);
+            Rigidbody2D rbAtaque = AtaqueInstanciada.GetComponent<Rigidbody2D>();
+            Vector3 direccion = (PuntoDisparo.position - (Vector3)transform.position).normalized;
+            rbAtaque.AddForce(direccion * 10f, ForceMode2D.Impulse);
+        }
+    }
+}
